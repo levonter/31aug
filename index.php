5a71040010f7208f25feee489d319cf0a2cfec55
System Administrator <root@Levents-MacBook-Pro.local>
2015-09-17 01:51:21 +0300

    initial changes

---
+++ index.php
@@ -0,0 +1,55 @@
+<?php
+require 'lib/connect.php';
+include 'inc/func.php';
+?>
+<!DOCTYPE html>
+<html lang="en,tr">
+
+<head>
+	<title>lev. ? shell@31aug.com || levent kaya</title>
+	
+	<!-- meta tags -->
+	<link rel="stylesheet" type="text/css" href="assets/css/normalize.css">
+	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+	<link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
+
+</head>
+
+<body>
+
+	<header class="col1-2">
+		<div class="logo col-3">
+
+			<!-- js animation -->
+
+		</div>
+	</header>
+
+
+	<!-- here we go -->
+	<div class="wrapper">
+
+		<div class="works col-12">			
+			<!-- col12 -->
+		</div>
+
+	</div> <!-- /wrapper -->
+
+
+	<footer>
+		
+
+	</footer>
+
+
+	<!-- scripts -->
+	<script type="text/javascript" src="assets/js/require.js"></script>
+	<script type="text/javascript" src="assets/js/analytics.js"></script>
+	<script type="text/javascript" src="assets/js/jquery.min.js"></script>
+	<script type="text/javascript" src="assets/js/bootstrap.min.js"></script>
+	<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.min.js"></script>
+	
+
+</body>
+
+</html>
